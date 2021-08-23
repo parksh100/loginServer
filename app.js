8ba710db512c3a1baf42fb5b9c7866187e33de61
shpark <spark616@gmail.com>
2021-08-23 11:39:17 +0900

    웹서버 띄우기

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000, (req, res) => {
+app.get("/", (req, res) => {
+  res.send("여기는 루트입니다");
+});
+
+app.get("/login", (req, res) => {
+  res.send("여기는 로그인입니다.");
+});
+
+app.listen(3000, () => {
   console.log("서버가동");
 });
