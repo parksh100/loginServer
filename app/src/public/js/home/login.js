79119acf1e318b013bbf310ff75daa3202edf7f7
shpark <spark616@gmail.com>
2021-08-24 10:58:32 +0900

    Dom을 이용하여 html객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use srict";
 
-console.log("hello");
+const id = document.querySelector("#id"),
+  psw = document.querySelector("#psw"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psw: psw.value,
+  };
+  console.log(req);
+}
