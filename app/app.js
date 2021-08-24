abab41f4d285d261f4422729a1fa94dd43fd10cb
shpark <spark616@gmail.com>
2021-08-24 10:25:06 +0900

    폴더구조 최적화3

--- app/app.js
+++ app/app.js
@@ -6,11 +6,11 @@ const express = require("express");
 const app = express();
 
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 //라우트 불러오기
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 app.use("/", home);
 
 module.exports = app;
--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const app = require("../app");
-const PORT = 3000;
-
-//포터 지정
-app.listen(PORT, () => {
-  console.log("서버가동");
-});
