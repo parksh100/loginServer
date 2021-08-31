3719adf5f06561e301e3ad9ce0f2a09085e8aa49
parksh100 <parksh1000@outlook.com>
2021-09-01 00:44:58 +0900

    깃공유테스트 문구

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
 //모듈
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 //앱 세팅
-app.set("views", "./src/views");
-app.set("view engine", "ejs");
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 //urlㅇ르 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제해결
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //라우트 불러오기
-const home = require("./src/routes/home");
-app.use("/", home);
+const home = require('./src/routes/home');
+app.use('/', home);
 
 module.exports = app;
+
+//test중임
