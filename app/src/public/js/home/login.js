e6da945d0f45a0d10cbaa808bff8239f35b612c1
shpark <spark616@gmail.com>
2021-09-01 15:21:14 +0900

    철자수정

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,10 +1,10 @@
-"use srict";
+'use strict';
 
-const id = document.querySelector("#id"),
-  psw = document.querySelector("#psw"),
-  loginBtn = document.querySelector("button");
+const id = document.querySelector('#id'),
+  psw = document.querySelector('#psw'),
+  loginBtn = document.querySelector('button');
 
-loginBtn.addEventListener("click", login);
+loginBtn.addEventListener('click', login);
 
 function login() {
   const req = {
@@ -13,10 +13,10 @@ function login() {
   };
   //여기까지는 프론트의 요청을 서버 콘솔에 보여주기만 하는 코드
   //여기서 부터는 fetch로 프론트 정보를 서버에 전달하는 코드...
-  fetch("/login", {
-    method: "POST",
+  fetch('/login', {
+    method: 'POST',
     headers: {
-      "content-type": "application/json", //json형태로 보낸다는 것을 알려줌
+      'content-type': 'application/json', //json형태로 보낸다는 것을 알려줌
     },
     body: JSON.stringify(req), //json화 함
   })
@@ -24,13 +24,13 @@ function login() {
     .then((res) => res.json())
     .then((res) => {
       if (res.success) {
-        location.href = "/";
+        location.href = '/';
       } else {
         alert(res.msg);
       }
     })
     .catch((err) => {
-      console.log(new Error("로그인 중 에러 발생"));
+      console.log(new Error('로그인 중 에러 발생'));
     });
 }
 //이상의 데이터를 서버에서 받을 수 있는 api가 마련되어 있어야 한다.
